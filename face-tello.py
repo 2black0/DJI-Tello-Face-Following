d0b4c368482e9b36fdda86d3009f1bbedc616abf
2black0 <2black0@gmail.com>
2021-06-26 20:36:44 +0700

    Update face-tello.py

--- face-tello.py
+++ face-tello.py
@@ -1,7 +1,6 @@
 import cv2
 from djitellopy import Tello
 import argparse
-import keyboard
 
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -71,7 +70,7 @@ if __name__=="__main__":
             myFrame = frame_read.frame
             img = cv2.resize(myFrame, args.vsize)
 
-            if keyboard.is_pressed('t') and status_flying is False:
+            if cv2.waitKey(1) & 0xFF == ord('t') and status_flying is False:
                 status_flying = True
                 tello.takeoff()
         else:
@@ -80,6 +79,8 @@ if __name__=="__main__":
             if not ret:
                 break
         
+        #cv2.imshow("Camera", img)
+        #lr_vel, fb_vel, up_vel, y_vel = [0, 0, 0, 0]
         lr_vel, fb_vel, up_vel, y_vel = showCam(img, args.vsize, args.tello, status_flying)
         
         if args.tello is True and status_flying is True:
@@ -87,7 +88,7 @@ if __name__=="__main__":
             if height > 30:
                 tello.send_rc_control(lr_vel, fb_vel, up_vel, y_vel)
 
-        if keyboard.is_pressed('l'):
+        if cv2.waitKey(1) & 0xFF == ord('l'):
             if args.tello is True:
                 tello.land()
                 tello.streamoff()
@@ -95,3 +96,13 @@ if __name__=="__main__":
                 cap.release()
             cv2.destroyAllWindows()
             break
+
+    '''cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    while True:
+        ret, img = cap.read()
+        cv2.imshow("Camera", img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break'''
