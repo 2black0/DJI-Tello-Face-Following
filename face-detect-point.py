fe2ae77929ef8f14b20f6898dc7efac6782389af
2black0 <2black0@gmail.com>
2021-06-26 09:19:12 +0700

    update

---
+++ face-detect-point.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import cv2
+
+def webcam_face_detect(video_mode, nogui = False, cascasdepath = "haarcascade_frontalface_default.xml"):
+    face_cascade = cv2.CascadeClassifier(cascasdepath)
+    video_capture = cv2.VideoCapture(video_mode)
+    num_faces = 0
+
+    while True:
+        ret, image = video_capture.read()
+        if not ret:
+            break
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+            minSize = (30,30)
+            )
+
+        print("The number of faces found = ", len(faces))
+        num_faces = len(faces)
+
+        if not nogui:
+            for (x,y,w,h) in faces:
+                cv2.circle(image, (320, 240), 2, (0, 0, 255), 2)
+                cv2.rectangle(image, (x,y), (x+h, y+h), (0, 255, 0), 2)
+                cv2.circle(image, (int((w/2)+x), int((h/2)+y)), 2, (0, 255, 0), 2)
+            cv2.imshow("Faces found", image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+    return num_faces
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        video_mode= 0
+    else:
+        video_mode = sys.argv[1]
+    webcam_face_detect(video_mode)
